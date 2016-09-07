5f26d954cc5e8cd9bb5c56a7f447f604a158a7e5
etech <excellencetechnologies>
2016-09-07 15:18:45 +0530

    check credientials and sidemenu added

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, MenuController} from 'ionic-angular';
 import {FormService } from './../../providers/form-service/form-service';
+import {LoginPage} from './../login/login'
+import { Data } from './../../components/data/data';
 @Component({
     templateUrl: 'build/pages/home/home.html',
     providers: [FormService]
 })
 export class HomePage implements OnInit {
-    lists:any;
-    constructor(private navCtrl: NavController, private menuCtrl: MenuController,private _formService: FormService) {
-
+    lists: any;
+    public data: Data[];
+    constructor(private navCtrl: NavController, private menuCtrl: MenuController, private _formService: FormService) {
+        
     }
     ngOnInit() {
         var path = { "parent_id": "1", "type": "full" }
         this._formService.api("category/categorylist/", path).subscribe((res) => {
             if (res) {
-                console.log(res);
                 this.lists = res.children;
-               // localStorage.setItem('lists', JSON.stringify(this.lists))
-              //  console.log(this.lists);
+                localStorage.setItem('lists', JSON.stringify(this.lists));
             }
         },
             (err) => {
@@ -31,5 +32,21 @@ export class HomePage implements OnInit {
     openMenu() {
         this.menuCtrl.open();
     }
-
+    toggle(data: Data) {
+        if (data.showDetails) {
+            data.showDetails = false;
+            data.icon = 'ios-add-circle-outline';
+        } else {
+            data.showDetails = true;
+            data.icon = 'ios-remove-circle-outline';
+        }
+    }
+    logout() {
+        localStorage.removeItem('firstname');
+        localStorage.removeItem('lastname');
+        localStorage.removeItem('expiry');
+        localStorage.removeItem('access_token');
+        this.navCtrl.push(LoginPage);
+    }
+   
 }
--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import { NavController, ToastController, Storage, LocalStorage } from 'ionic-angular';
 import {FORM_DIRECTIVES} from '@angular/common';
 import {FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {RegisterPage} from '../register/register'
@@ -14,11 +14,13 @@ export class LoginPage {
     logform: FormGroup;
     spin: boolean;
     response: string;
+    local: any;
     constructor(private navCtrl: NavController, private fb: FormBuilder, private _formService: FormService, public toastCtrl: ToastController) {
         this.logform = this.fb.group({
             email: ['', Validators.required],
             password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
         });
+        this.local = new Storage(LocalStorage);
     }
     gotoreg() {
         this.navCtrl.push(RegisterPage);
@@ -26,16 +28,17 @@ export class LoginPage {
     signin(logvalue: any) {
         this.spin = true;
         this._formService.api('customer/login/', logvalue).subscribe((res) => {
-            if (res) {
-                console.log(res);
-                this.spin = false;
-                this.navCtrl.push(HomePage);
-            }
+            this.local.set('firstname', res.firstname);
+            this.local.set('lastname', res.lastname);
+            this.local.set('access_token', res.access_token);
+            this.local.set('expiry', res.expiry);
+            this.spin = false;
+            this.navCtrl.push(HomePage);
         },
             (err) => {
                 if (err.status == 500) {
                     this.spin = false;
-                    this.response=JSON.parse(err._body).message;
+                    this.response = JSON.parse(err._body).message;
                     this.showToast("top");
                 }
 
