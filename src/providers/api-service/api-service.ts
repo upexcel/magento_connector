19762872463d4b5d8f1987b74c817998c037b20a
upexcel <rahul.excel2011@gmail.com>
2016-10-17 17:07:09 +0530

    subject implemented

--- src/providers/api-service/api-service.ts
+++ src/providers/api-service/api-service.ts
@@ -7,33 +7,37 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/toPromise';
-
+import {Subject} from 'rxjs/Rx';
 @Injectable()
 export class ApiService {
-    access_token: string;
-    constructor(private _local: Storage, private _http: Http) {
-        this._local.get('access_token').then((value: any) => {
-            this.access_token = value;
-        });
-    }
-    api(path: any, body: any) {
-        let api_url = config.api_Url + path;
-        let headers = new Headers({ 'Content-Type': config.content_type, 'APP_ID': config.APP_ID, 'Authorization': this.access_token });
-        let options = new RequestOptions({ headers: headers });
-        return this._http.post(api_url, JSON.stringify(body), options)
-            .map(this._extractData)
-            .catch(this._handleError);
+    constructor(private _local: Storage, private _http: Http) { }
+    api(path, body) {
+        var subject = new Subject();
+        this._local.get('access_token').then((value) => {
+            var self = this;
+            let api_url = config.api_Url + path;
+            let headers = new Headers({ 'Content-Type': config.content_type, 'APP_ID': config.APP_ID, 'Authorization': value });
+            let options = new RequestOptions({ headers: headers });
+            self._http.post(api_url, JSON.stringify(body), options)
+                .subscribe((res:Response) => {
+                    self._extractData(res,subject)
+                },self._handleError)
+        })
+        return subject;
+
     }
 
-    private _extractData(res: Response) {
+    private _extractData(res, subject) {
         if (res.status < 200 || res.status >= 300) {
             throw new Error('Bad response status: ' + res.status);
         }
         let body = res.json();
+        subject.next(body)
         return body;
     }
     private _handleError(error: any) {
+        let subject = new Subject();
         console.error('An error occurred', error);
-        return Observable.throw(error.message || error);
+        return subject.catch(error.message || error);
     }
 }
