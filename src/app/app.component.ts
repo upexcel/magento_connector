77dd36dfacb5d71befdf7883a831d6738c639d68
upexcel <rahul.excel2011@gmail.com>
2016-10-20 10:36:36 +0530

    app.component issue#35

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,32 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 import {Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {StartPage} from '../pages/startpage/startpage';
 import {HomePage} from '../pages/home/home';
 import { Storage } from '@ionic/storage';
 @Component({
-    template: `<ion-nav [root]="rootPage"></ion-nav>
+    template: `<ion-nav [root]="_rootPage"></ion-nav>
     <loading-modal id="loading"></loading-modal>`
 })
-export class MyApp {
-    private rootPage: any;
-    constructor(private platform: Platform, public storage: Storage) {
+export class MyApp implements OnInit{
+    private _rootPage: any;
+    constructor(private _platform: Platform, private _local: Storage) {
+    }
+    ngOnInit(){
         this.checkCredentials();
-        platform.ready().then(() => {
+        this._platform.ready().then(() => {
             StatusBar.styleDefault();
         });
     }
     checkCredentials() {
-        var name:string;
-        this.storage.get('firstname').then((value: any) => {
-             name = value;
-        });
         let access_token;
-        this.storage.get('access_token').then((res) => {
+        this._local.get('access_token').then((res) => {
             access_token = res;
-            if (access_token != null || name !=null) {
-                this.rootPage = HomePage;
+            if (access_token != null) {
+                this._rootPage = HomePage;
             } else {
-                this.rootPage = StartPage;
+                this._rootPage = StartPage;
             }
         });
-
     }
 }
