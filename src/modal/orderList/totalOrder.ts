f86beb0877ff20fa0b6dca2092c8c8e9a8979fe2
upexcel <rahul.excel2011@gmail.com>
2016-10-27 10:21:17 +0530

    orderlist model

--- src/modal/orderList/totalOrder.ts
+++ src/modal/orderList/totalOrder.ts
@@ -1,14 +1,13 @@
 import { Injectable, OnInit}    from '@angular/core';
 import {ApiService } from './../../providers/api-service/api-service';
 import {TotalOrderDataType  } from './../../pages/orderList/totalOrderDataType';
-declare let Promise: any;
 @Injectable()
 export class TotalOrder implements OnInit {
     constructor(private _apiService: ApiService) { }
     ngOnInit() { }
-    getTotalOrder(data): Promise<TotalOrderDataType> {
+    getTotalOrder(data) {
         let apiservice = this._apiService;
-        return new Promise(function(resolve, reject) {
+        return new Promise<TotalOrderDataType>(function(resolve, reject) {
             apiservice.api("order/totalorder", data).subscribe((res) => {
                 resolve(res);
             }, (err) => {
