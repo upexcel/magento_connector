bf759226640aa161004f224b1c3c75f57a100a88
upexcel <rahul.excel2011@gmail.com>
2016-10-27 18:32:52 +0530

    orderlist and orderid detail modified

--- src/model/orderList/orderlistDatatype.ts
+++ src/model/orderList/orderlistDatatype.ts
@@ -2,9 +2,9 @@ export class OrderListDataType{
   "data": {
       "order_id"?: string,
       "grand_total"?: string,
-      "created_at"?: string,
+      "created_at"?: any,
       "status"?: string
     };
-  "status"?: number;
-  "message"?: string;
+  "status": number;
+  "message": string;
 }
--- src/pages/orderid-detail/orderid-detail.ts
+++ src/pages/orderid-detail/orderid-detail.ts
@@ -6,45 +6,16 @@ import { Storage } from '@ionic/storage';
 import forEach from 'lodash/forEach';
 import clone from 'lodash/clone';
 import {OrderIdDetail} from './../../model/orderid-detail/orderid-detail';
+import { OrderIdDetailDataType } from './../../model/orderid-detail/orderid-detailData';
 @Component({
     templateUrl: 'orderid-detail.html'
 })
 
 export class OrderModalPage implements OnInit {
-    order_no: number;
+    orderid_detail:OrderIdDetailDataType;
     order_id: number;
-    customer_name: string;
-    customer_email: string;
-    purchased_on: any;
-    grand_total: number;
-    item: any;
-    status: string;
-    shipping_method: string;
-    payment_method: string;
-    state: string;
-    total_qty: number;
-    sub_total: number;
-    orders: string = "order_id";
-    items: any = [];
-    tax: number;
-    shipping_amount: number;
-    bill_fname: string;
-    bill_lname: string;
-    bill_telephone: number;
-    bill_city: string;
-    bill_region: string;
-    bill_street: Array<string>;
-    bill_country: string;
-    bill_postcode: number;
-    ship_fname: string;
-    ship_lname: string;
-    ship_telephone: number;
-    ship_city: string;
-    ship_region: string;
-    ship_street: Array<string>;
-    ship_country: string;
-    ship_postcode: string;
-    shipping_description: string;
+    item:any;
+    items:any;
     showOrder:boolean=false;
     showOrderError:boolean=false;
     constructor(private _orderdetail: OrderIdDetail, private _local: Storage, private _navparam: NavParams, private _popoverCtrl: PopoverController, private _viewCtrl: ViewController, private _apiService: ApiService) { }
@@ -62,15 +33,13 @@ export class OrderModalPage implements OnInit {
             order_id: order_id, secret: secret
         }
         this._orderdetail.getHomeProducts(body).then((res) => {
-          if(res.message==''){
+          this.orderid_detail=res;
+          if(this.orderid_detail.message==''){
             this.showOrderError=true;
           }else{
             this.showOrder=true;
-              this.status = res.data.status;
-              this.grand_total = res.data.grand_total;
-              this.purchased_on = res.data.purchased_on;
-              this.item = res.data.items;
-              this.payment_method = res.data.payment_method;
+            console.log(this.orderid_detail.data.items)
+              this.item =  this.orderid_detail.data.items;
               var res_data: any = [];
               forEach(this.item, function(value, key) {
                   var datas = {
@@ -80,28 +49,6 @@ export class OrderModalPage implements OnInit {
                   res_data.push(datas);
               })
               this.items = clone(res_data);
-              this.total_qty = res.data.total_qty_ordered;
-              this.sub_total = res.data.base_grand_total;
-              this.tax = res.data.tax_amount;
-              this.shipping_amount = res.data.shipping_amount;
-              this.bill_fname = res.data.billing_address.name;
-              this.bill_lname = res.data.billing_address.lastname;
-              this.bill_telephone = res.data.billing_address.telephone;
-              this.bill_city = res.data.billing_address.city;
-              this.bill_region = res.data.billing_address.region;
-              this.bill_street = res.data.billing_address.street;
-              this.bill_postcode = res.data.billing_address.postcode;
-              this.bill_country = res.data.billing_address.county;
-              this.ship_fname = res.data.shipping_address.name;
-              this.ship_lname = res.data.shipping_address.lastname;
-              this.ship_telephone = res.data.shipping_address.telephone;
-              this.ship_city = res.data.shipping_address.city;
-              this.ship_region = res.data.shipping_address.region;
-              this.ship_street = res.data.shipping_address.street;
-              this.ship_postcode = res.data.shipping_address.postcode;
-              this.ship_country = res.data.shipping_address.county;
-              this.shipping_method = res.data.shipping_method;
-              this.shipping_description = res.data.shipping_description;
           }
         }).catch((err)=>{
           console.log(err);
