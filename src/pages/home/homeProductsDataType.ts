78ed2900088e3cd990bfcb80856a93e4131d8957
excellencetechnologies <excellencetechnologies@gmail.com>
2016-11-15 13:29:37 +0530

    fix design but not tested on device

--- src/pages/home/homeProductsDataType.ts
+++ src/pages/home/homeProductsDataType.ts
@@ -7,19 +7,20 @@ export class HomeProductsDataType {
             name: string,
             type: string,
             sku: string,
-            weight ?: string,
+            currency: string,
+            weight?: string,
             price: string,
             display_price: string,
-            special_from_date ?: any,
-            special_to_date ?: any,
-            group_price ?: number,
-            special_price ?: string,
-            display_special_price ?: string,
-            tier_price ?: Array<string>,
-            media_images ?: Array<string>,
-            additional_information ?: Array<string>,
+            special_from_date?: any,
+            special_to_date?: any,
+            group_price?: number,
+            special_price?: string,
+            display_special_price?: string,
+            tier_price?: Array<string>,
+            media_images?: Array<string>,
+            additional_information?: Array<string>,
             in_stock: string,
-            show_on_home_page ?: string
+            show_on_home_page?: string
         }
         bundle_items?: {
             options: Array<any>
