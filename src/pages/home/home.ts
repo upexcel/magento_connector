ee8fac9d39a984552409a7862ce04f0165e4057e
upexcel <rahul.excel2011@gmail.com>
2016-11-18 11:06:57 +0530

    Fixed app exit button issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Events, NavController } from 'ionic-angular';
+import {Platform} from 'ionic-angular';
+import { Events, NavController, ViewController } from 'ionic-angular';
 import {HomeProductsDataType  } from './../../model/home/homeProductsDataType';
 import { HomeProducts } from '../../model/home/homeProducts';
 import slice from 'lodash/slice';
+import {ToastService} from './../../providers/toast-service/toastService';
 @Component({
     templateUrl: 'home.html'
 })
@@ -13,10 +15,10 @@ export class HomePage implements OnInit {
     start: number = 0;
     end: number = 4;
     backPressed: boolean = false;
-    constructor(private _events: Events, private _homeProductsConfig: HomeProducts, private _navCtrl: NavController) { }
+    constructor(private _toast:ToastService, private _platform: Platform, private _events: Events, private _homeProductsConfig: HomeProducts, private _navCtrl: NavController, private _viewController: ViewController) { }
     ngOnInit() {
         this.homeProducts();
-        this.registerBackButtonListener();
+        this.checkBackButton();
     }
     ionViewDidEnter() {
         setTimeout(() => { this._events.publish("title", { title: "Home", pagename: "home" }); }, 0);
@@ -32,6 +34,22 @@ export class HomePage implements OnInit {
                 this.spin = false;
             }
         })
+    }
+    checkBackButton() {
+        this._platform.registerBackButtonAction(()=> {
+                if(this._viewController.isLast() && this._viewController.isFirst()){
+                    if(!this.backPressed) {
+                        this.backPressed = true;
+                        this._toast.toast('Press Again For Exit App',3000,"top");
+                        setTimeout(() => this.backPressed = false, 2000);
+                        return;
+                    } else{
+                        navigator.app.exitApp()
+                    }
+                } else{
+                    this._navCtrl.pop();
+                }
+            }, 100);
     }
     doInfinite(infiniteScroll) {
         if (this.homeProduct.data.length % 2 == 0) {
@@ -71,20 +89,5 @@ export class HomePage implements OnInit {
         setTimeout(() => {
             refresher.complete();
         }, 2000);
-    }
-    registerBackButtonListener() {
-        console.log(this._navCtrl.parent);
-        document.addEventListener('backbutton', () => {
-                if(!this._navCtrl.parent){
-                    if(!this.backPressed) {
-                        this.backPressed = true;
-                        console.log('back')
-                        setTimeout(() => this.backPressed = false, 2000)
-                        return;
-                    } else{
-                        navigator.app.exitApp()
-                    }
-                }
-        });
     }
 }
