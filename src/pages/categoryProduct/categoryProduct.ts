9ec093942b69cb24a3666fbe1670fc395187c3b8
upexcel <rahul.excel2011@gmail.com>
2016-11-25 18:37:36 +0530

    changes in files

--- src/pages/categoryProduct/categoryProduct.ts
+++ src/pages/categoryProduct/categoryProduct.ts
@@ -16,7 +16,8 @@ export class CategoryProductPage implements OnInit {
     title: string;
     limit: number = 10;
     page: number = 1;
-    categoryProduct: CategoryProductDataType;
+    // categoryProduct: CategoryProductDataType;
+    categoryProduct: any;
     access_token:string;
     showPopOver:boolean=false;
     constructor(private _appConfigService: AppDataConfigService,private _events: Events,private _local:Storage,private _category: CategoryProduct, private _loadingCtrl: LoadingController, private _navCtrl: NavController, private _navParams: NavParams, private _menuCtrl: MenuController, private _popoverCtrl: PopoverController) {
@@ -43,24 +44,25 @@ export class CategoryProductPage implements OnInit {
         let body = { "id": product_id, "page": page, "limit": limit };
         this._category.getCategoryProduct(body).then((res) => {
             this.categoryProduct = res;
+            console.log(res);
         })
             .catch((err) => {
             });
     }
     doInfinite(infiniteScroll) {
         var limit = this.limit;
-        if (this.categoryProduct.data.length % 2 == 0) {
-            if (this.categoryProduct.data.length < limit) {
+        if (this.categoryProduct.length % 2 == 0) {
+            if (this.categoryProduct.length < limit) {
                 infiniteScroll.complete();
             }
-            else if (this.categoryProduct.data.length >= limit) {
+            else if (this.categoryProduct.length >= limit) {
                 setTimeout(() => {
                     this.limit += 6;
                     this.show_products(this.product_id, this.page, this.limit);
                     infiniteScroll.complete();
                 }, 2000);
             }
-            else if (this.categoryProduct.data.length <= limit) {
+            else if (this.categoryProduct.length <= limit) {
                 setTimeout(() => {
                     this.limit += 6;
                     this.show_products(this.product_id, this.page, this.limit);
