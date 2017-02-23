de1222cce5d07f1c75da98ae229c576d77674b26
deepakexcel <deepak.excel2011@gmail.com>
2017-02-23 13:48:48 +0530

    commited

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams, Events } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { CartFunction } from '../../model/cart/cartHandling'
-    
-
-
-
+import { ProductPage } from './../product/product'
 
 @Component({
     templateUrl: 'cart.html'
@@ -15,30 +12,33 @@ export class CartPage implements OnInit {
     lists: any = [];
     entery: boolean = false;
     totalPay: number;
-    constructor(private _cartFunction: CartFunction, private _events: Events, public local: Storage, public navCtrl: NavController, public navParams: NavParams) { }
+    constructor(private _cartFunction: CartFunction, private _events: Events, public local: Storage, public _navCtrl: NavController, public navParams: NavParams) { }
     ngOnInit() {
         this.local.get('CartData').then((value: any) => {
             console.log(value);
-            this.res = value;            
+            this.res = value;
             this.entery = true;
-//            this._cartFunction.totalPay(this.res).then((response) => {
-//                this.totalPay = response;
-//            });
+            //            this._cartFunction.totalPay(this.res).then((response) => {
+            //                this.totalPay = response;
+            //            });
         });
     }
 
 
     changeQuantity() {
         this._cartFunction.updateCart(this.res);
-//        this._cartFunction.totalPay(this.res).then((response) => {
-//            this.totalPay = response;
-//        });
+        //        this._cartFunction.totalPay(this.res).then((response) => {
+        //            this.totalPay = response;
+        //        });
     }
 
     deleteProduct(data) {
-        this._cartFunction.deleteItem(data).then((res)=>{
+        this._cartFunction.deleteItem(data).then((res) => {
             this.res = res;
         });
+    }
+    edit(data) {
+        this._navCtrl.push(ProductPage,{'id':data.sku});
     }
 
 }
