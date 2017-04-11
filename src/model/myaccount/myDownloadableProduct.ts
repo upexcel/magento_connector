fdc86a2e91b3bdc03b7f772978c61f1ff2c7dbfd
upexcel <rahul.excel2011@gmail.com>
2017-04-11 16:19:29 +0530

    Implement my downloadable product api + ui

--- src/model/myaccount/myDownloadableProduct.ts
+++ src/model/myaccount/myDownloadableProduct.ts
@@ -7,7 +7,7 @@ export class MyDownlodeData {
     getMyDownlodeData(data):Promise<MyAccountAddressDataType> {
         let apiservice = this._apiService;
         return new Promise(function(resolve, reject) {
-            apiservice.api("account/address/", data).subscribe((res) => {
+            apiservice.api("account/getdownloadable", data).subscribe((res) => {
                 resolve(res);
             }, (err) => {
                 reject(err);
--- src/pages/myaccount/downloadableProduct.ts
+++ src/pages/myaccount/downloadableProduct.ts
@@ -1,11 +1,33 @@
-
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {OrderModalPage} from '../orderid-detail/orderid-detail';
+import {MyDownlodeData} from './../../model/myaccount/myDownloadableProduct';
+import {AppDataConfigService} from './../../providers/appdataconfig/appdataconfig';
+import forEach from 'lodash/forEach';
 @Component({
-    selector: 'my-download',
-    templateUrl: 'download.html'
+   selector: 'my-download',
+   templateUrl: 'download.html'
 })
 export class Downloadable {
-    spin: boolean;
-
+   spin: boolean= true;
+   myDownloadableData:any;
+   constructor(public _navCtrl: NavController, public _myDownlodeData: MyDownlodeData, private _appConfigService: AppDataConfigService) {}
+   ngOnInit(){
+       this.spin = true;
+       this._appConfigService.getUserData().then((userData: any) => {
+            let secret = userData ? userData['secret'] : "";
+            this._myDownlodeData.getMyDownlodeData({"secret": secret}).then((res:any) => {
+                forEach(res['body']['data'], (value, key) => {
+                    value['date'] = (value.created_at.split(" "))[0];
+                    value['time'] = (value.created_at.split(" "))[1];
+                });
+                console.log(res.body);
+                this.myDownloadableData = res['body'];
+                this.spin = false;
+            })
+        });
+   }
+   viewOrderDetail(orderId) {
+       this._navCtrl.push(OrderModalPage, {"order_id": orderId});
+   }
 }
