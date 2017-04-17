c16b152bc841829c546007115baa6855939223df
upexcel <rahul.excel2011@gmail.com>
2017-04-17 13:06:43 +0530

    Fixed bug issues cart, address, wishlist

--- src/pages/wishList/wishList.ts
+++ src/pages/wishList/wishList.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
+import {Component} from '@angular/core';
+import {NavController, NavParams, LoadingController} from 'ionic-angular';
+import {Storage} from '@ionic/storage';
 import forEach from 'lodash/forEach';
-import { ProductPage } from '../../pages/product/product';
-import { WishListService } from '../../providers/wishList/wishList-service';
-import { HomePage } from './../home/home';
+import {ProductPage} from '../../pages/product/product';
+import {WishListService} from '../../providers/wishList/wishList-service';
+import {HomePage} from './../home/home';
+import {CartService} from './../../providers/cart-service/cart-service';
+import {ToastService} from './../../providers/toast-service/toastService';
+import {CartPage} from '../cart/cart';
+import { CartFunction } from '../../model/cart/cartHandling';
 
 @Component({
     selector: 'wishList',
@@ -12,8 +16,8 @@ import { HomePage } from './../home/home';
 })
 export class wishList {
     data: any;
-    constructor(public _wishListService: WishListService, public local: Storage, private _navParams: NavParams, public _nav: NavController) {}
-    ionViewWillEnter(){
+    constructor(private _cartFunction: CartFunction, public loadingCtrl: LoadingController, private _toast: ToastService, private _cartService: CartService, public _wishListService: WishListService, public local: Storage, private _navParams: NavParams, public _nav: NavController) {}
+    ionViewWillEnter() {
         this._wishListService.getWishList().then((data) => {
             this.data = data;
         })
@@ -24,11 +28,31 @@ export class wishList {
         })
     }
     editWishList(data) {
-        this._nav.push(ProductPage, { 'id': data.data.sku, "editCartData": data, "wishlist":true });
+        this._nav.push(ProductPage, {'id': data.data.sku, "editCartData": data, "wishlist": true});
     }
     addToCart(data) {
-        this._nav.push(ProductPage, {
-            id: data.data.sku
+        data['productid'] = data['productId'];
+        let loading = this.loadingCtrl.create({
+            content: 'Please wait...'
+        });
+        loading.present();
+        this._cartService.addCart(data, '').then((response: any) => {
+            loading.dismiss();
+            if (response.body['success']) {
+                this._cartFunction.setCart(response.body['success_data']);
+                this._toast.toast(data['name'] + "added to your shopping cart", 3000, "top");
+                this._nav.push(CartPage);
+            }
+            else {
+                this._nav.push(ProductPage, {
+                    id: data.data.sku
+                });
+            }
+        }, (err) => {
+            loading.dismiss();
+            this._nav.push(ProductPage, {
+                id: data.data.sku
+            });
         });
     }
     c_Shopping() {
