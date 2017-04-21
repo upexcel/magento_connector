870a884ac25f788627755b6b0edce04873bda3cd
deepakexcel <deepak.excel2011@gmail.com>
2017-04-21 16:16:43 +0530

    working on logout service

--- src/providers/logout/logout-service.ts
+++ src/providers/logout/logout-service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import {App} from 'ionic-angular';
 import {AppDataConfigService } from './../../providers/appdataconfig/appdataconfig';
+import { StartPage } from './../../pages/startpage/startpage';
+
 @Injectable()
 
 export class LogoutService {
-    constructor(private _dataConfigService: AppDataConfigService, private _local: Storage) { }
-    logout(msg, _navCtrl?) {
+    constructor(private app: App,private _dataConfigService: AppDataConfigService, private _local: Storage) { }
+    logout(msg,_navCtrl?) {
+        var nav = this.app.getActiveNav();
         this._dataConfigService.resetDataInService();
         this._local.clear();
+        nav.popToRoot(StartPage);
     }
 }
