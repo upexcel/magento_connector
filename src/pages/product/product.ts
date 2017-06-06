ff04db26c015b66270d916e53979942a8b24df0f
mohitTandon <mohitTandon>
2017-06-06 17:48:14 +0530

    fix issue of review page

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -84,6 +84,7 @@ export class ProductPage implements OnInit {
     quote_id: string;
     item_id: string;
     editProductQuantity: number;
+    reviewLoader:boolean=true;
     constructor(private _cartFunction: CartFunction, public loadingCtrl: LoadingController, public _socialSharing: SocialSharing, public _wishList: WishListModel, public _modalCtrl: ModalController, public _wishListService: WishListService, private viewCtrl: ViewController, private _tierPrice: TierPrice, private _notifyService: NotifyMe, private emailTest: FormBuilder, private _appConfigService: AppDataConfigService, private _toast: ToastService, public _events: Events, public _getProduct: Product, private _local: Storage, private _cartService: CartService, private _navCtrl: NavController, private _navParams: NavParams, private _apiService: ApiService) {
         this.logform = this.emailTest.group({ email: ['', Validators.required] });
         this._appConfigService.getUserData().then((userData: any) => {
@@ -111,7 +112,11 @@ export class ProductPage implements OnInit {
             this.products();
             //coll when any review is added 
             this._events.subscribe('api:review', (review) => {
-                this.products();
+                // this.products();
+                 this.reviewLoader=false;
+                    setTimeout(()=>{
+                         this.reviewLoader=true;
+                    });  
             });
         })
     }
